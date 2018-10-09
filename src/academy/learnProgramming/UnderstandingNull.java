da4fb1a883bc1b0e2f8303f2551531002eb94374
venu2804 <venu2804@gmail.com>
2018-10-09 19:48:49 +0100

    First Commit

---
+++ src/academy/learnProgramming/UnderstandingNull.java
@@ -0,0 +1,31 @@
+package academy.learnProgramming;
+
+
+import java.sql.SQLOutput;
+
+public class UnderstandingNull {
+    static Object myObject = new Object();
+    static Object myOtherObject;
+    static int myInt;
+
+
+    public static void main(String[] args) {
+
+
+        Object myLocalObject = new Object();
+        Object myAnotherLocalObject;
+
+
+        System.out.println("MyObject = " + myObject);
+        System.out.println("My Other object =" + myOtherObject);
+        System.out.println("MY INT = " + myInt);
+
+
+        System.out.println("My Local objetct = " + myLocalObject);
+       // System.out.println("My Another Local Object = " + myAnotherLocalObject);
+
+    }
+
+
+
+}
